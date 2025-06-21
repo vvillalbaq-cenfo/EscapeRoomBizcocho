0144e303f78afb0a3343bc4ddfd1ff5ce045b4d7
Victor Villalba <vvillalbaq@ucenfotec.ac.cr>
2025-06-21 23:45:57 +0000

    S02

---
+++ EscapeRoomBizcocho/src/cuartoUno/CodigoCuartoUno.java
@@ -0,0 +1,9 @@
+package cuartoUno;
+import utilitarios.utils;
+public class CodigoCuartoUno {
+    public static void main(String[] args) {
+        //Limpiar la consola
+        utils.limpiarConsola();
+    }
+    
+}
