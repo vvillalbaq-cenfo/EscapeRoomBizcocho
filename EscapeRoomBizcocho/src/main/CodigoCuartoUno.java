df7149c6de14c81d2df8f02591bbd6a60b7384b1
Victor Villalba <vvillalbaq@ucenfotec.ac.cr>
2025-06-24 13:36:30 +0000

    S02

--- EscapeRoomBizcocho/src/main/CodigoCuartoUno.java
+++ EscapeRoomBizcocho/src/main/CodigoCuartoUno.java
@@ -1,7 +1,13 @@
-package cuartoUno;
-import utilitarios.utils;
+package main;
+import utilitarios.Utils;
+
 public class CodigoCuartoUno {
     public static void main(String[] args) {
-      
+      //Limpiar la consola
+      Utils.limpiarConsola();
+
+      if (inicioPrograma == true) {
+        // Aquí va el código para el cuarto uno
+      }
     }
 }
--- EscapeRoomBizcocho/src/main/InicioUsuario.java
+++ EscapeRoomBizcocho/src/main/InicioUsuario.java
@@ -1,26 +1,26 @@
 package main;
 
-import utilitarios.utils;
+import utilitarios.Utils;
 
 public class InicioUsuario {
     public static void main(String[] args) {
         // Limpiar la consola
-        utils.limpiarConsola();
+        Utils.limpiarConsola();
 
-        // variables
-        String nombreUsuario = utils.leerTexto();
-        boolean inicioJuego = false;
-        // Entrada de usuario
+        // Entrada de Bienvenida
         System.out.println("Bienvenido al Escape Room Bizcocho");
         System.out.println("Por favor, ingresa tu nombre de usuario:");
+        String nombreUsuario = Utils.leerString(""); // Solicita el nombre
+        
 
-        // Validar entrada Y Salida de Bienvenida 
-        if (nombreUsuario.isEmpty()) {
+        // Validar entrada Y Salida de Bienvenida
+        if (nombreUsuario == null || nombreUsuario.isEmpty()) {
             System.out.println("El nombre de usuario no puede estar vacío. Por favor, intenta de nuevo.");
-        } else {
+        } else { 
             System.out.println("¡Hola " + nombreUsuario + "! Estas preparado para lograr escpar del Bizcocho?");
-            inicioJuego = true;
+            boolean inicioPrograma = true;
         }
       
+        
     }
 }
