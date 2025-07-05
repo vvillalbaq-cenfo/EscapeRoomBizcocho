b596fa4010288f1364c344969f6e9c592a16f906
Victor Villalba <vvillalbaq@ucenfotec.ac.cr>
2025-07-05 18:56:19 +0000

    S02

--- EscapeRoomBizcocho/src/main/CodigoCuartoUno.java
+++ EscapeRoomBizcocho/src/main/CodigoCuartoUno.java
@@ -31,13 +31,14 @@ public class CodigoCuartoUno {
         numeroSeleccion = Utils.leerEntero("¿A qué objeto te quieres dirigir? (escribe el número).");
 
         // Validacion y seleccion de Elementos Habitacion
-   switch (numeroSeleccion) {
+        switch (numeroSeleccion) {
           case 1:
             if (numeroSeleccion == 1) {
               if (puertaMetalica == false) {
-                System.out.println("La puerta está cerrada con un código de 7 cifras. Debes encontrar las pistas para tu sorpresa especial y poder salir de este Escape Room.");
+                System.out.println(
+                    "La puerta está cerrada con un código de 7 cifras. Debes encontrar las pistas para tu sorpresa especial y poder salir de este Escape Room.");
                 puertaMetalica = true;
-                tiempo = tiempo -0.5;
+                tiempo = tiempo - 0.5;
                 System.out.println("Te quedan " + tiempo + " minutos.");
               } else {
                 System.out.println("La puerta sigue cerrada, necesitas el código correcto para abrirla.");
@@ -60,68 +61,83 @@ public class CodigoCuartoUno {
             }
             break;
           case 3:
-      System.out.println("Estos son los elementos que puedes revisar:");
-        System.out.println("1. Informe Médico");
-        System.out.println("2. Llave");
-        System.out.println("3. Caja Fuerte");
+            do {
 
-            numeroSeleccionEscritorio = Utils.leerEntero("¿A qué objeto te quieres dirigir? (escribe el número).");
+              System.out.println("Estos son los elementos que puedes revisar en el escritorio:");
+              System.out.println("1. Informe Médico");
+              System.out.println("2. Llave");
+              System.out.println("3. Caja Fuerte");
 
-            switch (numeroSeleccionEscritorio) {
-              case 1:
-                if (numeroSeleccionEscritorio == 1) {
-                  if (informeMedico == false) {
-                    System.out.println(
-                        "El informe médico dice: 'Paciente con amnesia temporal. Necesita encontrar pistas para recordar su identidad y salir del cuarto'.");
-                    System.out.println("El informe médico incluye el número de paciente resaltado el cual es #W3792");
-                    informeMedico = true;
-                    tiempo = tiempo - 0.5;
-                    System.out.println("Te quedan " + tiempo + " minutos.");
-                  } else {
-                    System.out.println("Ya revisaste el informe médico, no hay nada más que hacer aquí.");
-                    tiempo = tiempo - 0.5;
-                  }
-                }
-                break;
-              case 2:
-                if (numeroSeleccionEscritorio == 2) {
-                  if (llave == false) {
-                    System.out.println("Encontraste la Llave, debajo de ella hay una nota con la letra H.");
-                    llave = true;
-                    tiempo = tiempo - 0.5;
-                    System.out.println("Te quedan " + tiempo + " minutos.");
-                  } else {
-                    System.out.println("Ya revisaste la llave, no hay nada más que hacer aquí.");
-                    tiempo = tiempo - 0.5;
-                  }
-                }
-                break;
-              case 3:
-                if (numeroSeleccionEscritorio == 3) {
-                  if (cajaFuerte == false) {
-                    System.out.println("Esta es una caja fuerte debajo del escritorio, está cerrada y necesita un código de 4 números para poder abrirla. Busca la llave para abrirla.");
-                    cajaFuerte = true;
-                    tiempo = tiempo - 0.5;
-                    System.out.println("Te quedan " + tiempo + " minutos.");
-                    codigoCajaBoolean = Utils.leerBoolean("Sabes cual es el codigo de la caja fuerte? (4 dígitos)");
-                    if (codigoCajaBoolean == true) {
-                      codigoCorrecto = Utils.leerEntero("Escribe el código de la caja fuerte (4 dígitos).");
+              numeroSeleccionEscritorio = Utils.leerEntero("¿A qué objeto te quieres dirigir? (escribe el número).");
+
+              switch (numeroSeleccionEscritorio) {
+                case 1:
+                  if (numeroSeleccionEscritorio == 1) {
+                    if (informeMedico == false) {
+                      System.out.println(
+                          "El informe médico dice: 'Paciente con amnesia temporal. Necesita encontrar pistas para recordar su identidad y salir del cuarto'.");
+                      System.out.println("El informe médico incluye el número de paciente resaltado el cual es #W3792");
+                      informeMedico = true;
+                      tiempo = tiempo - 0.5;
+                      System.out.println("Te quedan " + tiempo + " minutos.");
+                      loopEscritorio = Utils.leerBoolean("¿Quieres seguir revisando el escritorio? (true/false)");
 
                     } else {
-                      System.out.println("No sabes el código de la caja fuerte, sigue buscando pistas.");
-
+                      System.out.println("Ya revisaste el informe médico, no hay nada más que hacer aquí.");
+                      loopEscritorio = Utils.leerBoolean("¿Quieres seguir revisando el escritorio? (true/false)");
+                      tiempo = tiempo - 0.5;
                     }
-                  } else {
-                    System.out.println("Ya revisaste la caja fuerte, no hay nada más que hacer aquí.");
-                    tiempo = tiempo - 0.5;
                   }
-                }
-                break;
+                  break;
+                case 2:
+                  if (numeroSeleccionEscritorio == 2) {
+                    if (llave == false) {
+                      System.out.println("Encontraste la Llave, debajo de ella hay una nota con la letra H.");
+                      llave = true;
+                      tiempo = tiempo - 0.5;
+                      System.out.println("Te quedan " + tiempo + " minutos.");
+                      loopEscritorio = Utils.leerBoolean("¿Quieres seguir revisando el escritorio? (true/false)");
+                    } else {
+                      System.out.println("Ya revisaste la llave, no hay nada más que hacer aquí.");
+                      loopEscritorio = Utils.leerBoolean("¿Quieres seguir revisando el escritorio? (true/false)");
+                      tiempo = tiempo - 0.5;
+                    }
+                  }
+                  break;
+                case 3:
+                  if (numeroSeleccionEscritorio == 3) {
+                    if (cajaFuerte == false) {
+                      System.out.println(
+                          "Esta es una caja fuerte debajo del escritorio, está cerrada y necesita un código de 4 números para poder abrirla. Busca la llave para abrirla.");
+                      cajaFuerte = true;
+                      tiempo = tiempo - 0.5;
+                      System.out.println("Te quedan " + tiempo + " minutos.");
+                      loopEscritorio = Utils.leerBoolean("¿Quieres seguir revisando el escritorio? (true/false)");
 
-              default:
-                System.out.println("Opción no válida. Por favor, elige un número entre 1 y 3.");
-                break;
-            }
+                      if (loopEscritorio == false) {
+                        codigoCajaBoolean = Utils.leerBoolean("Sabes cual es el codigo de la caja fuerte? (4 dígitos)");
+                        if (codigoCajaBoolean == true) {
+                          codigoCorrecto = Utils.leerEntero("Escribe el código de la caja fuerte (4 dígitos).");
+                        } else {
+                          System.out.println("No sabes el código de la caja fuerte, sigue buscando pistas.");
+                          loopEscritorio = Utils.leerBoolean("¿Quieres seguir revisando el escritorio? (true/false)");
+                          tiempo = tiempo - 0.5;
+                        }
+                      }
+                    } else {
+                      System.out.println("Ya revisaste la caja fuerte, no hay nada más que hacer aquí.");
+                      tiempo = tiempo - 0.5;
+                      loopEscritorio = Utils.leerBoolean("¿Quieres seguir revisando el escritorio? (true/false)");
+                    }
+                  }
+                  break;
+
+                default:
+                  System.out.println("Opción no válida. Por favor, elige un número entre 1 y 3.");
+                  loopEscritorio = Utils.leerBoolean("¿Quieres seguir revisando el escritorio? (true/false)");
+                  break;
+              }
+            } while (loopEscritorio == true);
 
             break;
           default:
@@ -129,16 +145,12 @@ public class CodigoCuartoUno {
             break;
         }
       }
-      
-    
-
-    if (tiempo == 0) {
-      System.out
-          .println("Tiempo agotado! Game Over! El Bizcocho te ha atrapado! Entró el gas mortífero y te aniquiló.");
-    } else {
-      System.out.println(
-          "¡Felicidades! Has encontrado el código correcto y has escapado del cuarto. ¡Eres un verdadero héroe del Escape Room Bizcocho!");
+      if (tiempo == 0) {
+        System.out
+            .println("Tiempo agotado! Game Over! El Bizcocho te ha atrapado! Entró el gas mortífero y te aniquiló.");
+      } else {
+        System.out.println("Oh! Oh! De la caja fuerte sale un gas que te hace sentir mareado y te desmayas.");
+      }
     }
-  }
   }
 }
