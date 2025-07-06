353c65fc104ed0faa628b031bed2cb8d524092d1
Victor Villalba <vvillalbaq@ucenfotec.ac.cr>
2025-07-06 00:13:09 +0000

    S02

---
+++ EscapeRoomBizcocho/src/main/CodigoCuartoDos.java
@@ -0,0 +1,156 @@
+package main;
+
+import utilitarios.Utils;
+
+public class CodigoCuartoDos {
+  public static void main(String[] args) {
+    Utils.limpiarConsola();
+
+    // Variables de juego
+    double tiempo = 10;
+    int nivelActual = 2;
+    int intentosCaja = 5;
+    int codigoCaja = 3792;
+    boolean camaraFalsa = false, escritorio = false, colchon = false, cajaFuerte = false;
+    boolean puertaMetalica = false, botellaAgua = false, rasgaduraColchon = false;
+    boolean pared = false, pizarra = false, lamparaEncendida = true;
+    String informeMedico = "", notaLlave = "", notaMojada = "", codigoParcial = "";
+
+    System.out.println("Te despiertas en un cuarto oscuro...");
+    System.out.println("Tienes 10 minutos para escapar antes de que el gas tóxico te mate.");
+
+    while (tiempo > 0 && !puertaMetalica) {
+      System.out.println("\nNivel " + nivelActual + " | Tiempo restante: " + tiempo + " minutos");
+      System.out.println("¿Qué quieres examinar?");
+      System.out.println("1. Cámara");
+      System.out.println("2. Escritorio");
+      System.out.println("3. Colchón");
+      System.out.println("4. Pared");
+      System.out.println("5. Botella de Agua");
+      System.out.println("6. Caja Fuerte");
+      System.out.println("7. Pizarra");
+      System.out.println("0. Esperar");
+
+      int opcion = Utils.leerEntero("Elige una opción (0-7):");
+
+      switch (opcion) {
+        case 1:
+          if (!camaraFalsa) {
+            System.out.println("Examinas la cámara. Está rota. Detrás hay una flecha al escritorio y la letra 'O'.");
+            camaraFalsa = true;
+            codigoParcial += "O";
+          } else {
+            System.out.println("Ya examinaste la cámara.");
+          }
+          tiempo -= 0.5;
+          break;
+
+        case 2:
+          if (!escritorio) {
+            System.out.println("Encuentras un folder, una caja fuerte, una llave, una lámpara vieja y una botella de agua.");
+            escritorio = true;
+            informeMedico = "Paciente #W3792 Fecha: 12/04";
+            System.out.println("El folder contiene: " + informeMedico);
+          } else {
+            System.out.println("Ya examinaste el escritorio.");
+          }
+          tiempo -= 0.5;
+          break;
+
+        case 3:
+          if (!colchon) {
+            System.out.println("Examinas el colchón. Parece rasgado. Hay una nota, pero no puedes leerla sin luz.");
+            colchon = true;
+          } else {
+            System.out.println("Ya examinaste el colchón.");
+          }
+
+          if (!rasgaduraColchon && lamparaEncendida) {
+            System.out.println("Con la luz de la lámpara ves una línea: 'Busca lo que está roto'. Encuentras la letra 'E'.");
+            rasgaduraColchon = true;
+            codigoParcial += "E";
+          }
+          tiempo -= 0.5;
+          break;
+
+        case 4:
+          if (!pared) {
+            System.out.println("Encuentras una grieta con una anotación: 'El agua revela la verdad y te regalo una (R)'.");
+            pared = true;
+            codigoParcial += "R";
+          } else {
+            System.out.println("Ya examinaste la pared.");
+          }
+          tiempo -= 0.5;
+          break;
+
+        case 5:
+          if (!botellaAgua) {
+            System.out.println("Tomas la botella de agua. Contiene una nota mojada que dice '29 - P'.");
+            botellaAgua = true;
+            notaMojada = "29P";
+            codigoParcial += "P";
+            System.out.println("Nota mojada encontrada: " + notaMojada);
+          } else {
+            System.out.println("Ya revisaste la botella de agua.");
+          }
+          tiempo -= 0.5;
+          break;
+
+        case 6:
+          if (!cajaFuerte) {
+            boolean cajaAbierta = false;
+            for (; intentosCaja > 0 && !cajaAbierta; intentosCaja--) {
+              int intento = Utils.leerEntero("Ingresa el código de la caja fuerte (" + intentosCaja + " intentos restantes):");
+              if (intento == codigoCaja) {
+                System.out.println("Código correcto. Encuentras una llave con la letra 'H'. La puerta se abre.");
+                notaLlave = "H";
+                codigoParcial += "H";
+                puertaMetalica = true;
+                cajaAbierta = true;
+              } else {
+                System.out.println("Código incorrecto.");
+              }
+            }
+
+            if (!puertaMetalica && intentosCaja == 0) {
+              System.out.println("Se agotaron los intentos. La caja fuerte permanece cerrada.");
+            }
+
+            cajaFuerte = true;
+          } else {
+            System.out.println("Ya intentaste abrir la caja fuerte.");
+          }
+          tiempo -= 0.5;
+          break;
+
+        case 7:
+          if (!pizarra) {
+            System.out.println("Encuentras una pizarra con el código '2973 (bigc)'.");
+            pizarra = true;
+          } else {
+            System.out.println("Ya examinaste la pizarra.");
+          }
+          tiempo -= 0.5;
+          break;
+
+        case 0:
+          System.out.println("Decides esperar un momento. El tiempo pasa.");
+          tiempo -= 0.5;
+          break;
+
+        default:
+          System.out.println("Opción no válida. Intenta nuevamente.");
+          break;
+      }
+    }
+
+    System.out.println();
+    if (puertaMetalica) {
+      System.out.println("¡Escapaste con éxito!");
+      System.out.println("Código recolectado: " + codigoParcial);
+    } else {
+      System.out.println("El gas tóxico te alcanzó. El tiempo se acabó.");
+    }
+  }
+}
