9973c6931c780f0cc78a24dbed2b0adee344b4d7
Victor Villalba <vvillalbaq@ucenfotec.ac.cr>
2025-08-03 17:14:03 +0000

    S02

---
+++ EscapeRoomBizcocho/src/main/MainEscapeRoom.java
@@ -0,0 +1,15 @@
+public class MainEscapeRoom {
+    public static void main(String[] args) {
+
+    }
+    public static void iniciarJuego() {
+
+    }
+
+    public static void codigoCuartoUno(String codigo1) {
+
+    }
+
+    
+
+}
